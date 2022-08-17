c8d7c9f3261eeed2fe3c110df0e18f2f9a76ea6f
walexkino papy <waladino@yahoo.com>
2022-08-17 21:26:30 +0100

    Deployed the frontend and backend for phase 3

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,10 @@
+export  TYPEORM_CONNECTION=postgres
+export  TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+export  TYPEORM_HOST=database-1.cyixof8rdrny.us-east-1.rds.amazonaws.com
+export  TYPEORM_PORT=5432
+export  TYPEORM_USERNAME=postgres
+export  TYPEORM_PASSWORD=postgres
+export  TYPEORM_DATABASE=postgres
+export  TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export  TYPEORM_MIGRATIONS_DIR=./src/migrations
+
